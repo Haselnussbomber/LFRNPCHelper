06781ea001e1e40d8f55e7b39ebed7ae0d83bfdb
Haselnussbomber <mail@haselnussbomber.de>
2020-04-10 22:49:32 +0200

    initial commit

---
+++ LFRNPCHelper.lua
@@ -0,0 +1,243 @@
+local npcData = {
+
+	-- Auridormi (Cataclysm)
+	[80675] = {
+		-----------------------
+		-- Dragon Soul (187) --
+		-----------------------
+
+		-- Option 1 | The Siege of Wyrmrest Temple: Morchok (311), Warlord Zon'ozz (324), Yor'sahj the Unsleeping (325), Hagara the Stormbinder (317)
+		{ 187, { 311, 324, 325, 317 } },
+
+		-- Option 2 | Fall of Deathwing: Ultraxion (331), Warmaster Blackhorn (332), Spine of Deathwing (318), Madness of Deathwing (333)
+		{ 187, { 331, 332, 318, 333 } },
+	},
+
+
+	-- Lorewalker Han (Mists of Pandaria)
+	[80633] = {
+		----------------------------
+		-- Mogu'shan Vaults (317) --
+		----------------------------
+
+		-- Option 1 | Guardians of Mogu'shan: The Stone Guard (679), Feng the Accursed (689), Gara'jal the Spiritbinder (682)
+		{ 317, { 679, 689, 682 } },
+
+		-- Option 2 | The Vault of Mysteries: The Spirit Kings (687), Elegon (726), Will of the Emperor (677)
+		{ 317, { 687, 726, 677 } },
+
+		-------------------------
+		-- Heart of Fear (330) --
+		-------------------------
+
+		-- Option 3 | The Dread Approach: Imperial Vizier Zor'lok (745), Blade Lord Ta'yak (744), Garalon (713)
+		{ 330, { 745, 744, 713 } },
+
+		-- Option 4 | Nightmare of Shek'zeer: Wind Lord Mel'jarak (741), Amber-Shaper Un'sok (737), Grand Empress Shek'zeer (743)
+		{ 330, { 741, 737, 743 } },
+
+		-------------------------------------
+		-- Terrace of Endless Spring (320) --
+		-------------------------------------
+	
+		-- Option 5 | Protectors of the Endless (683), Tsulong (742), Lei Shi (729), Sha of Fear (709)
+		{ 320, { 683, 742, 729, 709 } },
+
+		-----------------------------
+		-- Throne of Thunder (362) --
+		-----------------------------
+
+		-- Option 6 | Last Stand of the Zandalari: Jin'rokh the Breaker (827), Horridon (819), Council of Elders (816)
+		{ 362, { 827, 819, 816 } },
+
+		-- Option 7 | Forgotten Depths: Tortos (825), Megaera (821), Ji-Kun (828)
+		{ 362, { 825, 821, 828 } },
+
+		-- Option 8 | Halls of Flesh-Shaping: Durumu the Forgotten (818), Primordius (820), Dark Animus (824)
+		{ 362, { 818, 820, 824 } },
+
+		-- Option 9 | Pinnacle of Storms: Iron Qon (817), Twin Consorts (829), Lei Shen (832)
+		{ 362, { 817, 829, 832 } },
+
+		------------------------------
+		-- Siege of Orgrimmar (369) --
+		------------------------------
+
+		-- Option 10 | Vale of Eternal Sorrows: Immerseus (852), The Fallen Protectors (849), Norushen (866), Sha of Pride (867)
+		{ 369, { 852, 849, 866, 867 } },
+
+		-- Option 11 | Gates of Retribution: Galakras (868), Iron Juggernaut (864), Kor'kron Dark Shaman (856), General Nazgrim (850)
+		{ 369, { 868, 864, 856, 850 } },
+
+		-- Option 12 | The Underhold: Malkorok (846), Spoils of Pandaria (870), Thok the Bloodthirsty (851)
+		{ 369, { 846, 870, 851 } },
+
+		-- Option 13 | Downfall: Siegecrafter Blackfuse (865), Paragons of the Klaxxi (853), Garrosh Hellscream (869)
+		{ 369, { 865, 853, 869 } },
+	},
+
+
+	-- Seer Kazal (Warlords of Draenor)
+	[94870] = {
+		--------------------
+		-- Highmaul (477) --
+		--------------------
+
+		-- Option 1 | Walled City: Kargath Bladefist (1128), The Butcher (971), Brackenspore (1196)
+		{ 477, { 1128, 971, 1196 } },
+
+		-- Option 2 | Arcane Sanctum: Tectus (1195), Twin Ogron (1148), Ko'ragh (1153)
+		{ 477, { 1195, 1148, 1153 } },
+
+		-- Option 3 | Imperator's Rise: Imperator Mar'gok (1197)
+		{ 477, { 1197 } },
+
+		-----------------------------
+		-- Blackrock Foundry (457) --
+		-----------------------------
+
+		-- Option 4 | Slagworks: Gruul (1161), Oregorger (1202), The Blast Furnace (1154)
+		{ 457, { 1161, 1202, 1154 } },
+
+		-- Option 5 | The Black Forge: Hans'gar and Franzok (1155), Flamebender Ka'graz (1123), Kromog (1162)
+		{ 457, { 1155, 1123, 1162 } },
+
+		-- Option 6 | Iron Assembly: Beastlord Darmac (1122), Operator Thogar (1147), The Iron Maidens (1203)
+		{ 457, { 1122, 1147, 1203 } },
+
+		-- Option 7 | Blackhand's Crucible: Blackhand (959)
+		{ 457, { 959 } },
+
+		----------------------------
+		-- Hellfire Citadel (669) --
+		----------------------------
+
+		-- Option 8 | Hellbreach: Hellfire Assault (1426), Iron Reaver (1425), Kormrok (1392)
+		{ 669, { 1426, 1425, 1392 } },
+
+		-- Option 9 | Halls of Blood: Hellfire High Council (1432), Kilrogg Deadeye (1396), Gorefiend (1372)
+		{ 669, { 1432, 1396, 1372 } },
+
+		-- Option 10 | Bastion of Shadows: Shadow-Lord Iskar (1433), Socrethar the Eternal (1427), Tyrant Velhari (1394)
+		{ 669, { 1433, 1427, 1394 } },
+
+		-- Option 11 | Destructor's Rise: Fel Lord Zakuun (1391), Xhul'horac (1447), Mannoroth (1395)
+		{ 669, { 1391, 1447, 1395 } },
+
+		-- Option 12 | The Black Gate: Archimonde (1438)
+		{ 669, { 1438 } },
+	},
+
+
+	-- Archmage Timear (Legion)
+	[111246] = {
+		---------------------------------
+		-- The Emerald Nightmare (768) --
+		---------------------------------
+
+		-- Option 1 | Darkbough: Nythendra (1703), Elerethe Renferal (1744), Il'gynoth (1738)
+		{ 768, { 1703, 1744, 1738 } },
+
+		-- Option 2 | Tormented Guardians: Ursoc (1667), Dragons of Nightmare (1704), Cenarius (1750)
+		{ 768, { 1667, 1704, 1750 } },
+
+		-- Option 3 | Rift of Aln: Xavius (1726)
+		{ 768, { 1726 } },
+
+		-------------------------
+		-- The Nighthold (786) --
+		-------------------------
+
+		-- Option 4 | Arcing Aqueducts: Skorpyron (1706), Chronomatic Anomaly (1725), Trilliax (1731)
+		{ 786, { 1706, 1725, 1731 } },
+
+		-- Option 5 | Royal Athenaeum: Spellblade Aluriel (1751), Star Augur Etraeus (1732), High Botanist Tal'arn (1761)
+		{ 786, { 1751, 1732, 1761 } },
+
+		-- Option 6 | Nightspire: Krosus (1713), Tichondrius (1762), Elisande (1743)
+		{ 786, { 1713, 1762, 1743 } },
+
+		-- Option 7 | Betrayer's Rise: Gul'dan (1737)
+		{ 786, { 1737 } },
+
+		--------------------------
+		-- Trial of Valor (861) --
+		--------------------------
+
+		-- Option 8 | Odyn (1819), Guarm (1830), Helya (1829)
+		{ 861, { 1819, 1830, 1829 } },
+
+		----------------------------
+		-- Tomb of Sargeras (875) --
+		----------------------------
+
+		-- Option 9 | The Gates of Hell: Goroth (1862), Harjatan (1856), Mistress Sassz'ine (1861)
+		{ 875, { 1862, 1856, 1861 } },
+
+		-- Option 10 | Wailing Halls: Demonic Inquisition (1867), Sisters of the Moon (1903), The Desoalte Host (1896)
+		{ 875, { 1867, 1903, 1896 } },
+
+		-- Option 11 | Chamber of the Avatar: Maiden of Vigilance (1897), Fallen Avatar (1873)
+		{ 875, { 1897, 1873 } },
+
+		-- Option 12 | Deceiver's Fall: Kil'jaeden (1898)
+		{ 875, { 1898 } },
+
+		---------------------------------------
+		-- Antorus, the Burning Throne (946) --
+		---------------------------------------
+
+		-- Option 13 | Light's Breach: Garothi Worldbreaker (1992), Felhounds of Sargeras (1987), Antoran High Command (1997)
+		{ 946, { 1992, 1987, 1997 } },
+
+		-- Option 14 | Forbidden Descent: Portal Keeper Hasabel (1985), Eonar the Life-binder (2025), Imonar the Soulhunter (2009)
+		{ 946, { 1985, 2025, 2009 } },
+
+		-- Option 15 | Hope's End: Kin'garoth (2004), Varimathras (1983), Coven of Shivarra (1986)
+		{ 946, { 2004, 1983, 1986 } },
+
+		-- Option 16 | Seat of the Pantheon: Aggrammar (1984), Argus the Unmaker (2031)
+		{ 946, { 1984, 2031 } },
+	},
+
+}
+
+hooksecurefunc("GossipFrameOptionsUpdate", function(...)
+	if not UnitExists("npc") then return end
+
+	local guid = UnitGUID("npc")
+	if not guid then return end
+
+	local npcID = tonumber(guid:match("-(%d+)-%x+$"), 10) or nil
+	if not npcID then return end
+
+	local overrides = npcData[npcID]
+	if not overrides then return end
+
+	local titleIndex = 1
+	for i=1, select("#", ...), 2 do
+		local titleButton = _G["GossipTitleButton" .. titleIndex]
+		local override = overrides[titleButton:GetID()]
+		if override then
+			local instanceId = override[1]
+			local encounterIds = override[2]
+
+			local str = ""
+			for j=1, #encounterIds do
+				if str ~= "" then str = str .. "\n" end
+
+				local name, _, _, _, _, _, dungeonEncounterID, mapID = EJ_GetEncounterInfo(encounterIds[j])
+				str = str .. " - " .. name
+
+				if C_RaidLocks.IsEncounterComplete(mapID, dungeonEncounterID, instanceId < 369 and 7 or 17) then
+					str = str .. " (" .. DUNGEON_ENCOUNTER_DEFEATED .. ")"
+				end
+			end
+
+			local instanceName = EJ_GetInstanceInfo(instanceId)
+			titleButton:SetText("[" .. instanceName .. "]\n" .. str)
+			GossipResize(titleButton)
+		end
+		titleIndex = titleIndex + 1
+	end
+end)
